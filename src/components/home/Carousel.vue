6e4a8f8a8e30da1febda86b8d801bd013cd37637
ChrisRM <chris.ricardo.r.m@gmail.com>
2020-11-24 13:55:27 -0600

    :art: Add carousel images

--- src/components/home/Carousel.vue
+++ src/components/home/Carousel.vue
@@ -1,51 +1,60 @@
 <template>
-  <v-carousel
-    cycle
-    height="500"
-    hide-delimiter-background
-    show-arrows-on-hover
-  >
-    <v-carousel-item
-      v-for="(slide, i) in slides"
-      :key="i"
-    >
-      <v-sheet
-        :color="colors[i]"
-        height="100%"
-      >
-        <v-row
-          class="fill-height"
-          align="center"
-          justify="center"
-        >
-          <div class="display-3">
-            {{ slide }} Slide
-          </div>
-        </v-row>
-      </v-sheet>
+  <v-carousel cycle height="500" hide-delimiter-background show-arrows-on-hover>
+    <v-carousel-item v-for="(slide, i) in slides" :key="i" :src="slide.sale">
+      <v-row class="fill-height" align="end" justify="center">
+        <div class="display-3 font-weight-medium pb-10">
+          <v-sheet
+            :class="`${colors[i]}`"
+            class="mx-auto transition-swing rounded-lg px-1 cursor"
+            elevation="12"
+            >{{ slide.order }}
+          </v-sheet>
+        </div>
+      </v-row>
     </v-carousel-item>
   </v-carousel>
 </template>
 <script>
+import storeSale from "../../assets/store-sale.webp";
+import hatsSale from "../../assets/hats-sale.jpg";
+import clothingSale from "../../assets/clothing-sale.jpg";
+import bookSale from "../../assets/book-sale.webp";
+import blackfridaySale from "../../assets/black-friday-sale.webp";
+
 export default {
   name: "Carousel",
-  data () {
-      return {
-        colors: [
-          'indigo',
-          'warning',
-          'pink darken-2',
-          'red lighten-1',
-          'deep-purple accent-4',
-        ],
-        slides: [
-          'First',
-          'Second',
-          'Third',
-          'Fourth',
-          'Fifth',
-        ],
-      }
-    }
+  data() {
+    return {
+      colors: [
+        "indigo",
+        "orange darken-3",
+        "pink darken-2",
+        "red lighten-1",
+        "deep-purple accent-4",
+      ],
+      slides: [
+        {
+          sale: storeSale,
+          order: "Just browsing? Check these cool items!",
+        },
+        {
+          sale: hatsSale,
+          order: "Hats on sale, this week",
+        },
+        {
+          sale: clothingSale,
+          order: "Popular clothing items",
+        },
+        {
+          sale: bookSale,
+          order: "Books, books, books",
+        },
+        {
+          sale: blackfridaySale,
+          order: "Black friday sale",
+        },
+      ],
+    };
+  },
 };
 </script>
